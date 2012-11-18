4969b861051d90c225210ea18fe348970ad9c6c4
Baldur Rensch <baldur.rensch@hautelook.com>
2012-11-17 18:06:16 -0800

    Updated namespace
    Added dependency to SF HTTP-Kernel

--- Component/Routing/Generator/Rfc6570Generator.php
+++ Component/Routing/Generator/Rfc6570Generator.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Hautelook\ApiBundle\Component\Routing\Generator;
+namespace Hautelook\TemplatedUriBundle\Component\Routing\Generator;
 
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
