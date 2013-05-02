9512b8328654a3dd81feb6e6d1e464591ead75bf
Baldur Rensch <baldur.rensch@hautelook.com>
2013-05-01 18:41:10 -0700

    Fixed braces that should not be encoded

--- Tests/Routing/Generator/Rfc6570GeneratorTest.php
+++ Tests/Routing/Generator/Rfc6570GeneratorTest.php
@@ -50,6 +50,7 @@ class Rfc6570GeneratorTest extends \PHPUnit_Framework_TestCase
         return array(
             array('/foo/foobar/?{&bar}', array('foo' => 'foobar', 'bar' => 'barbar')),
             array('/foo/foobar/?{&bar%5B%5D*}', array('foo' => 'foobar', 'bar' => array())),
+            array('/foo/{placeholder}/?{&bar}', array('foo' => '{placeholder}', 'bar' => 'barbar')),
         );
     }
 
