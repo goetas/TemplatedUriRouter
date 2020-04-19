3174a90bf71e0f25fd3297970c30da3377f84994
Asmir Mustafic <goetas@gmail.com>
2020-04-19 08:19:02 +0200

    do not rely on HttpKernel to understand if we have a compiled router available

--- Routing/Generator/BcUrlGenerator.php
+++ Routing/Generator/BcUrlGenerator.php
@@ -3,13 +3,15 @@
 namespace Hautelook\TemplatedUriRouter\Routing\Generator;
 
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\Generator\CompiledUrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
-if (Kernel::MAJOR_VERSION < 5) {
-    abstract class BcUrlGenerator extends \Symfony\Component\Routing\Generator\UrlGenerator
+if (!class_exists('Symfony\Component\Routing\Generator\CompiledUrlGenerator')) {
+    abstract class BcUrlGenerator extends UrlGenerator
     {
     }
 } else {
-    abstract class BcUrlGenerator extends \Symfony\Component\Routing\Generator\CompiledUrlGenerator
+    abstract class BcUrlGenerator extends CompiledUrlGenerator
     {
     }
 }
